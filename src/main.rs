55e4f4103f2ab7605a44f600852c8b07b7d77a0d
jaehafe <wogkdkrm112@gmail.com>
2026-01-24 13:59:27 +0900

    feat: log init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)                                                  
+        .init();
+
+    tracing::info!("🚀 Starting server");
+
+    Ok(())
 }
